352bfc8cc855f5c754884864f97726ee62c33826
marpl136 <marpl136@student.liu.se>
2013-02-14 20:33:47 +0100

    Terminado hasta el paso 5 con los datos del tutorial...buscando datos nuevos para hacer nuestra versión.

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -1,6 +1,7 @@
 package com.google.gwt.sample.stockwatcher.client;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -8,6 +9,10 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
@@ -26,6 +31,7 @@ public class StockWatcher implements EntryPoint {
   private Button addStockButton = new Button("Add");
   private Label lastUpdatedLabel = new Label();
   private ArrayList<String> stocks = new ArrayList<String>();
+  private static final int REFRESH_INTERVAL = 5000; // ms
 
   /**
    * Entry point method.
@@ -51,6 +57,19 @@ public class StockWatcher implements EntryPoint {
 
     // Move cursor focus to the input box.
     newSymbolTextBox.setFocus(true);
+    
+
+    
+ // Setup timer to refresh list automatically.
+    Timer refreshTimer = new Timer() {
+      @Override
+      public void run() {
+        refreshWatchList();
+      }
+
+
+    };
+    refreshTimer.scheduleRepeating(REFRESH_INTERVAL);
 
     // Listen for mouse events on the Add button.
     addStockButton.addClickHandler(new ClickHandler() {
@@ -72,6 +91,7 @@ public class StockWatcher implements EntryPoint {
     
   }
   
+
   
   /**
    * Add stock to FlexTable. Executed when the user clicks the addStockButton or
@@ -110,9 +130,75 @@ public class StockWatcher implements EntryPoint {
 	    });
 	    stocksFlexTable.setWidget(row, 3, removeStockButton);
 
-	    // TODO Get the stock price.
+	 // Get the stock price.
+	    refreshWatchList();
 
 
   }
 
+  /**
+   * Generate random stock prices.
+   */
+  private void refreshWatchList() {
+    final double MAX_PRICE = 100.0; // $100.00
+    final double MAX_PRICE_CHANGE = 0.02; // +/- 2%
+
+   
+    FortuneDatas[] prices = new FortuneDatas[stocks.size()];
+    for (int i = 0; i < stocks.size(); i++) {
+      double price = Random.nextDouble() * MAX_PRICE;
+      double change = price * MAX_PRICE_CHANGE
+          * (Random.nextDouble() * 2.0 - 1.0);
+
+      prices[i] = new FortuneDatas(stocks.get(i), price, change);
+    }
+
+    updateTable(prices);
+    
+  }
+  
+	  /**
+	   * Update the Price and Change fields all the rows in the stock table.
+	   *
+	   * @param prices Stock data for all rows.
+	   */
+	  private void updateTable(FortuneDatas[] prices) {
+	    for (int i = 0; i < prices.length; i++) {
+	      updateTable(prices[i]);
+	    }
+
+	    // Display timestamp showing last refresh.
+	    lastUpdatedLabel.setText("Last update : "
+	        + DateTimeFormat.getMediumDateTimeFormat().format(new Date()));
+
+	  }
+	  
+	  /**
+	   * Update a single row in the stock table.
+	   *
+	   * @param price Stock data for a single row.
+	   */
+	  private void updateTable(FortuneDatas price) {
+	    // Make sure the stock is still in the stock table.
+	    if (!stocks.contains(price.getSymbol())) {
+	      return;
+	    }
+
+	    int row = stocks.indexOf(price.getSymbol()) + 1;
+
+	    // Format the data in the Price and Change fields.
+	    String priceText = NumberFormat.getFormat("#,##0.00").format(
+	        price.getPrice());
+	    NumberFormat changeFormat = NumberFormat.getFormat("+#,##0.00;-#,##0.00");
+	    String changeText = changeFormat.format(price.getChange());
+	    String changePercentText = changeFormat.format(price.getChangePercent());
+
+	    // Populate the Price and Change fields with new data.
+	    stocksFlexTable.setText(row, 1, priceText);
+	    stocksFlexTable.setText(row, 2, changeText + " (" + changePercentText
+	        + "%)");
+	  }
 }
+
+
+
