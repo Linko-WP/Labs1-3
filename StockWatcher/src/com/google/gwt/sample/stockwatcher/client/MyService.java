5ef0c2230da4a965b9e3bc28294528da4eca0652
Fran <frava356@student.liu.se>
2013-03-13 01:46:46 +0100

    Included calls to insert_into_db from outside of the server

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/MyService.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/MyService.java
@@ -6,4 +6,5 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 @RemoteServiceRelativePath("ms")
 public interface MyService extends RemoteService {
   public String initialize_db(String s);
+  public String insert_into_db(String table, String values);
 }
--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/MyServiceAsync.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/MyServiceAsync.java
@@ -4,4 +4,5 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 interface MyServiceAsync {
   public void initialize_db(String s, AsyncCallback<String> callback);
+  public void insert_into_db(String table, String values, AsyncCallback<String> callback);
 }
